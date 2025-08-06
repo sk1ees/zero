dd9ee59359b94a6409f46c896fef62ce50a38dab
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2025-08-06 23:01:50 +0530

    -- fixed build issue

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -329,7 +329,7 @@ const Login = () => {
           {/* Sign Up Link */}
           <div className="text-center">
             <p className="text-sm text-muted-foreground">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link
                 href="/signup"
                 className="text-primary hover:text-primary/80 font-medium"
