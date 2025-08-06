93e8eebb3ed03b3059dbc3d7542a845230e58a78
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2025-08-06 22:58:07 +0530

    -- added redirect login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-  <div className="flex items-center justify-center h-screen">  
-    <div className="text-md font-normal text-gray-700 blur-sm hover:blur-none transition-all duration-1000 ease-in-out">
-      zero.ai is coming soon.
-    </div>
-  </div>
-  );
+  redirect('/login');
 }
